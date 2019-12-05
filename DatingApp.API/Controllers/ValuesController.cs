0db570f30d06d86f3cc8083d0a6e699161af71f4
RedaSobh <RedaSobha@gmail.com>
2019-12-05 21:38:53 +0300

    End Of Section 3

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers {
     //Get http://localhost:5000/api/values/5
+    [Authorize]
     [Route ("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase {
         private readonly DataContext _context;
         public ValuesController (DataContext context) {
             _context = context;
-
         }
         // GET api/values
         [HttpGet]
@@ -24,6 +25,7 @@ namespace DatingApp.API.Controllers {
         }
 
         // GET api/values/5
+        [AllowAnonymous]
         [HttpGet ("{id}")]
         public async Task<IActionResult> GetValue (int id) {
             var value =await _context.Values.FirstOrDefaultAsync(x=>x.Id==id);
---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required][StringLength(8,MinimumLength=4,ErrorMessage="You Must Specify Password between 4 and 8 chars")]
+        public string Password { get; set; }
+    }
+}
